1aa2923df64fb19dd956230d6b7ff8b0b6dd2894
Oluwaseyi Egunjobi <seyistry@gmail.com>
2023-02-16 00:25:24 +0100

    fix resp

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -46,8 +46,11 @@ def create_place_id():
         return make_response(jsonify({"error": "Not a JSON"}), 400)
     if 'name' not in request.get_json():
         return make_response(jsonify({"error": "Missing name"}), 400)
-    js = request.get_json()
-    user_obj = Place(**js)
+    if 'user_id' not in request.get_json():
+        return make_response(
+            jsonify({"error": "Missing user_id"}), 400)
+    kwarg = request.get_json()
+    user_obj = Place(**kwarg)
     user_obj.save()
     return jsonify(user_obj.to_dict()), 201
 
@@ -62,7 +65,8 @@ def update_place_id(place_id):
     if user_obj is None:
         abort(404)
     for key, value in request.get_json().items():
-        if key not in ['id', 'created_at']:
+        if key not in ['id', 'user_id', 'city_id',
+                       'created_at', 'updated_at']:
             setattr(user_obj, key, value)
     storage.save()
     return jsonify(user_obj.to_dict())
