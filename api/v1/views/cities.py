e4546c02d39622788a01fb1d06785b5617d5dfcd
Oluwaseyi Egunjobi <seyistry@gmail.com>
2023-02-15 23:39:38 +0100

    fix id type to string

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -60,7 +60,7 @@ def create_city(state_id):
     return jsonify(city_obj.to_dict()), 201
 
 
-@app_views.route('/cities/<city_id>', methods=['PUT'],
+@app_views.route('/cities/<string:city_id>', methods=['PUT'],
                  strict_slashes=False)
 def update_city(city_id):
     """update city in state """
